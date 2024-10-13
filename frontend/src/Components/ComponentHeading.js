45a10fad3e2afec2694808cf50484c4deccf7de0
Sagar Sharma <sagarsharma1832@gmail.com>
2024-10-13 23:00:07 +0530

    new

--- frontend/src/Components/ComponentHeading.js
+++ frontend/src/Components/ComponentHeading.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 import { FaLongArrowAltRight } from "react-icons/fa";
-function ComponentHeading({mainHeading , viewMoreButton}) {
+function ComponentHeading({ mainHeading, viewMoreButton }) {
   return (
     <>
-    <div className='border-b-4 gap-1 border-grey-500 flex items-center justify-between w-full pb-0.5'>
-    <p className='font-bold sm:text-lg text-sm'>{mainHeading}</p>
-    <p className='flex gap-2 items-center text-xs font-medium text-gray-600  '>{viewMoreButton} <FaLongArrowAltRight /></p>
-    </div>
+      <div className="border-b-4 gap-1 border-grey-500 flex items-center justify-between w-full pb-0.5">
+        <p className="font-bold text-black sm:text-lg text-sm">{mainHeading}</p>
+        <p className="flex gap-2 items-center text-xs font-medium text-gray-600  ">
+          {viewMoreButton} <FaLongArrowAltRight />
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default ComponentHeading
+export default ComponentHeading;
