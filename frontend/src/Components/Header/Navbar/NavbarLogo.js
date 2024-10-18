3c1a229e3b024e3bc0a675d9a0bd97d10a0f46d6
Sagar Sharma <sagarsharma1832@gmail.com>
2024-10-19 01:00:42 +0530

    new changes

--- frontend/src/Components/Header/Navbar/NavbarLogo.js
+++ frontend/src/Components/Header/Navbar/NavbarLogo.js
@@ -4,7 +4,7 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 function NavbarLogo() {
   return (
-   <div className='flex gap-10 items-center'>
+   <div className='flex sm:gap-10 gap-5 items-center'>
      <RxHamburgerMenu className='lg:hidden text-lg' style={{color:"white"}}/>
   <img alt='store logo' className='max-w-full w-6.5/12 lg:w-9/12 lg:h-16 h-6 sm:h-8 ' src={img}/>
    </div>
